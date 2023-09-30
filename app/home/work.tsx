a1d760ca3fb7f63c6ff68a9cf36577d4ad32957d
jovand3v <jovand3v@gmail.com>
2023-09-30 17:15:14 +0200

    added work section

--- app/home/work.tsx
+++ app/home/work.tsx
@@ -15,6 +15,33 @@ const Work = () => {
         images={[test, test2, test, test2, test, test2]}
         reverse={true}
       />
+      <WorkProjectShowcase
+        title="KeepTrack"
+        subtitle="2021 - Designed & Coded (Personal Project)"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus velit justo, quis vestibulum enim facilisis in. Donec efficitur pretium tortor sit amet tincidunt. Mauris laoreet dictum mollis. Etiam sollicitudin tortor quam, a dictum felis interdum a. Donec facilisis consequat enim ac consequat. Nunc quis dolor volutpat, vehicula nisi vel, mollis sem. Cras nec nibh quis turpis varius vestibulum eget a elit."
+        liveLink="https://keep-track.netlify.app/"
+        githubLink="https://github.com/Dremiq/KeepTrack"
+        images={[test, test2, test, test2, test, test2]}
+        reverse={false}
+      />
+      <WorkProjectShowcase
+        title="KeepTrack"
+        subtitle="2021 - Designed & Coded (Personal Project)"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus velit justo, quis vestibulum enim facilisis in. Donec efficitur pretium tortor sit amet tincidunt. Mauris laoreet dictum mollis. Etiam sollicitudin tortor quam, a dictum felis interdum a. Donec facilisis consequat enim ac consequat. Nunc quis dolor volutpat, vehicula nisi vel, mollis sem. Cras nec nibh quis turpis varius vestibulum eget a elit."
+        liveLink="https://keep-track.netlify.app/"
+        githubLink="https://github.com/Dremiq/KeepTrack"
+        images={[test, test2, test, test2, test, test2]}
+        reverse={true}
+      />
+      <WorkProjectShowcase
+        title="KeepTrack"
+        subtitle="2021 - Designed & Coded (Personal Project)"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus velit justo, quis vestibulum enim facilisis in. Donec efficitur pretium tortor sit amet tincidunt. Mauris laoreet dictum mollis. Etiam sollicitudin tortor quam, a dictum felis interdum a. Donec facilisis consequat enim ac consequat. Nunc quis dolor volutpat, vehicula nisi vel, mollis sem. Cras nec nibh quis turpis varius vestibulum eget a elit."
+        liveLink="https://keep-track.netlify.app/"
+        githubLink="https://github.com/Dremiq/KeepTrack"
+        images={[test, test2, test, test2, test, test2]}
+        reverse={false}
+      />
     </div>
   );
 };
