62f210f633068f4727c681a0cab9d098470962e6
jovand3v <jovand3v@gmail.com>
2023-10-11 13:18:13 +0200

    updated project descriptions

--- app/home/work.tsx
+++ app/home/work.tsx
@@ -9,7 +9,7 @@ const Work = () => {
       <WorkProjectShowcase
         title="PHERRA"
         subtitle="2023 - Desgined & Coded"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus velit justo, quis vestibulum enim facilisis in. Donec efficitur pretium tortor sit amet tincidunt. Mauris laoreet dictum mollis. Etiam sollicitudin tortor quam, a dictum felis interdum a. Donec facilisis consequat enim ac consequat."
+        description="PHERRA is a personal full-stack, e-commerce project with a custom built cms that can be accessed via the /admin route. PHERRA is hosted on Vercel and was built using: Next.js, TypeScript, SCSS, JWT, Prisma, Vercel Postgres Database, AWS S3 Bucket, and Figma."
         liveLink="https://pherra.vercel.app/"
         githubLink="https://github.com/Dremiq/PHERRA"
         images={images.pherra}
@@ -17,8 +17,8 @@ const Work = () => {
       />
       <WorkProjectShowcase
         title="PORTFOLIO"
-        subtitle="2022 - Designed & Coded"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus velit justo, quis vestibulum enim facilisis in. Donec efficitur pretium tortor sit amet tincidunt. Mauris laoreet dictum mollis. Etiam sollicitudin tortor quam, a dictum felis interdum a. Donec facilisis consequat enim ac consequat."
+        subtitle="2023 - Designed & Coded"
+        description="My portfolio is a personal project used for representation of my work, skills and experiences. It's hosted on Vercel and was built using: Next.js, TypeScript, SCSS and Figma."
         liveLink="/"
         githubLink="https://github.com/Dremiq/portfolio"
         images={images.portfolio}
@@ -26,8 +26,8 @@ const Work = () => {
       />
       <WorkProjectShowcase
         title="MASHDAPP"
-        subtitle="2023 - Partially Designed & Coded"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus velit justo, quis vestibulum enim facilisis in. Donec efficitur pretium tortor sit amet tincidunt. Mauris laoreet dictum mollis. Etiam sollicitudin tortor quam, a dictum felis interdum a. Donec facilisis consequat enim ac consequat."
+        subtitle="2022 - Partially Designed & Coded"
+        description={`MASHDAPP is a work full-stack project made for customizing/combining NFTs. It was built by a team of 3. I've handled all of the front-end, hosting, some design tweaks, and some back-end such as the contact us form. MASHDAPP is hosted on Heroku and built(my side) using: React, Express, SCSS, Mailgun, Blockfrost, and Figma. On the last few images a MASHDAPP Editor can be seen that got discontinued and therefore never made it to production.`}
         liveLink="https://www.mashd.app/"
         githubLink={null}
         images={images.mashdapp}
@@ -36,7 +36,7 @@ const Work = () => {
       <WorkProjectShowcase
         title="KEEPTRACK"
         subtitle="2021 - Designed & Coded"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus velit justo, quis vestibulum enim facilisis in. Donec efficitur pretium tortor sit amet tincidunt. Mauris laoreet dictum mollis. Etiam sollicitudin tortor quam, a dictum felis interdum a. Donec facilisis consequat enim ac consequat."
+        description="KEEPTRACK is a personal front-end, nutrition tracking project. It's hosted on Netlify and was built using: React, Redux, SCSS, chart.js, and Figma. This project was initially built for personal use only but later I've decided to re-design and brand it, and put it as one of my main projects even if it was built in 2021."
         liveLink="https://keep-track.netlify.app/"
         githubLink="https://github.com/Dremiq/KeepTrack"
         images={images.keep_track}
